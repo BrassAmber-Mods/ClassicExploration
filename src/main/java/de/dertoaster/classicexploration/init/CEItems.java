e810c8f3dfa6d1eef691087ac3a44e8b6994f557
DerToaster98 <seiler.oli@web.de>
2021-07-26 17:14:43 +0200

    add generic items

--- src/main/java/de/dertoaster/classicexploration/init/CEItems.java
+++ src/main/java/de/dertoaster/classicexploration/init/CEItems.java
@@ -2,9 +2,17 @@ package de.dertoaster.classicexploration.init;
 
 import de.dertoaster.classicexploration.ClassicExplorationMod;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.Properties;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class CEItems {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ClassicExplorationMod.MODID);
+	
+	public static final RegistryObject<Item> ITEM_ENGINE = ITEMS.register("engine", () -> new Item(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64)));
+	public static final RegistryObject<Item> ITEM_BALLOON = ITEMS.register("balloon", () -> new Item(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64)));
+	public static final RegistryObject<Item> ITEM_CANNON = ITEMS.register("balloon", () -> new Item(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(1)));
+	//public static final RegistryObject<Item> ITEM_AIRSHIP = ITEMS.register("airship_engine", () -> new Item(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64)));
 }
