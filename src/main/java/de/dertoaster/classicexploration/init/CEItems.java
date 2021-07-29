9509686023665d6736f8983e48e8758f16e40144
DerToaster98 <seiler.oli@web.de>
2021-07-29 21:18:02 +0200

    add base item class that allows lore text

--- src/main/java/de/dertoaster/classicexploration/init/CEItems.java
+++ src/main/java/de/dertoaster/classicexploration/init/CEItems.java
@@ -1,6 +1,7 @@
 package de.dertoaster.classicexploration.init;
 
 import de.dertoaster.classicexploration.ClassicExplorationMod;
+import de.dertoaster.classicexploration.objects.items.ItemLore;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
 import net.minecraft.item.ItemGroup;
@@ -11,8 +12,8 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class CEItems {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ClassicExplorationMod.MODID);
 	
-	public static final RegistryObject<Item> ITEM_ENGINE = ITEMS.register("engine", () -> new Item(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64)));
-	public static final RegistryObject<Item> ITEM_BALLOON = ITEMS.register("balloon", () -> new Item(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64)));
-	public static final RegistryObject<Item> ITEM_CANNON = ITEMS.register("cannon", () -> new Item(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(1)));
+	public static final RegistryObject<Item> ITEM_ENGINE = ITEMS.register("engine", () -> new ItemLore(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64)));
+	public static final RegistryObject<Item> ITEM_BALLOON = ITEMS.register("balloon", () -> new ItemLore(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64)));
+	public static final RegistryObject<Item> ITEM_CANNON = ITEMS.register("cannon", () -> new ItemLore(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(1)));
 	//public static final RegistryObject<Item> ITEM_AIRSHIP = ITEMS.register("airship_engine", () -> new Item(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64)));
 }
---
+++ src/main/java/de/dertoaster/classicexploration/objects/items/ItemLore.java
@@ -0,0 +1,33 @@
+package de.dertoaster.classicexploration.objects.items;
+
+import java.util.List;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+public class ItemLore extends Item {
+	
+	public ItemLore(Properties p_i48487_1_) {
+		super(p_i48487_1_);
+	}
+
+	@Override
+	public void appendHoverText(ItemStack p_77624_1_, World p_77624_2_, List<ITextComponent> tooltip, ITooltipFlag p_77624_4_) {
+		super.appendHoverText(p_77624_1_, p_77624_2_, tooltip, p_77624_4_);
+		if (GLFW.glfwGetKey(Minecraft.getInstance().getWindow().getWindow(), GLFW.GLFW_KEY_LEFT_SHIFT) != GLFW.GLFW_PRESS) {
+			tooltip.add(new StringTextComponent(TextFormatting.BLUE + I18n.get("item." + this.getRegistryName().getNamespace() + ".tooltip.hold_shift", '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n')));
+		} else {
+			tooltip.add(new StringTextComponent(TextFormatting.BLUE + I18n.get("item." + this.getRegistryName().getNamespace() + "." + this.getRegistryName().getPath() + ".tooltip", '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n')));
+		}
+	}
+
+}
