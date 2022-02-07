c548c04282bfc7148afe863e668f6ed8f687b54a
DerToaster98 <seiler.oli@web.de>
2022-02-07 17:23:52 +0100

    add-in item tags

---
+++ src/main/java/de/dertoaster/classicexploration/init/CEItemTags.java
@@ -0,0 +1,12 @@
+package de.dertoaster.classicexploration.init;
+
+import de.dertoaster.classicexploration.ClassicExplorationMod;
+import net.minecraft.item.Item;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.ItemTags;
+
+public class CEItemTags {
+	
+	public static final ITag.INamedTag<Item> CHESTS = ItemTags.bind(ClassicExplorationMod.MODID + ":chests");
+
+}
